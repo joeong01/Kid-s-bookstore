a71fddab068ea55bf3ba6d560d397b19d49d2d35
terenceteoh23 <tteoh23@yahoo.com>
2022-10-19 00:37:57 +0800

    implented add to cart

--- book-details.php
+++ book-details.php
@@ -6,6 +6,28 @@ $bookInfoSql = "SELECT * FROM books WHERE bookID='$book_id'";
 $bookInfoResult = mysqli_query($con,$bookInfoSql);
 if(mysqli_num_rows($bookInfoResult) > 0)
     $book_info = mysqli_fetch_assoc($bookInfoResult);
+
+$book_price = $book_info['price'];
+
+$custID = $_SESSION["id"];
+
+if(isset($_GET['add'])){
+    $getCartSql = "SELECT cartID FROM shoppingCart WHERE customerID=$custID";
+
+    $result = mysqli_query($con, $getCartSql);
+
+    if(mysqli_num_rows($result) > 0){
+        $row = mysqli_fetch_assoc($result);
+    }
+    
+    $cart_id = $row['cartID'];
+
+    $sqlquery = "INSERT INTO shoppingCartDetails (cartID,bookID,numberOfBooks,totalPriceOfOne) 
+    VALUES ($cart_id,$book_id,1,$book_price)";
+
+    mysqli_query($con, $sqlquery);
+}
+
 ?>
 
 <main>
@@ -39,8 +61,10 @@ if(mysqli_num_rows($bookInfoResult) > 0)
                                         <br>
                                         <p>Published On: <?php echo $book_info['publishedDate']; ?></p>
                                     </div>
-                                    <a href="#" class="white-btn mr-10">Add to Cart</a>
-                                    <a href="#" class="border-btn share-btn"><i class="fas fa-share-alt"></i></a>
+                                    <?php
+                                        echo '<a href="book-details.php?id='.$book_info['bookID'].'&add=true" class="white-btn mr-10">Add to Cart</a>'
+                                    ?>
+                                    <button class="border-btn share-btn"><i class="fas fa-share-alt"></i></button>
                                 </div>
                             </div>
                         </div>
@@ -111,101 +135,19 @@ if(mysqli_num_rows($bookInfoResult) > 0)
     </section>
     <!-- Subscribe Area End -->
 </main>
-<footer>
-    <div class="footer-wrappper section-bg">
-     <div class="footer-area footer-padding">
-         <div class="container">
-             <div class="row justify-content-between">
-                 <div class="col-xl-3 col-lg-5 col-md-4 col-sm-6">
-                     <div class="single-footer-caption mb-50">
-                         <div class="single-footer-caption mb-30">
-                             <!-- logo -->
-                             <div class="footer-logo mb-25">
-                                 <a href="index.html"><img src="assets/img/logo/logo2_footer.png" alt=""></a>
-                             </div>
-                             <div class="footer-tittle">
-                                 <div class="footer-pera">
-                                     <p>Get the breathing space now, and we’ll extend your term at the other end year for go.</p>
-                                 </div>
-                             </div>
-                             <!-- social -->
-                             <div class="footer-social">
-                                <a href="https://bit.ly/sai4ull"><i class="fab fa-facebook"></i></a>
-                                <a href="#"><i class="fab fa-instagram"></i></a>
-                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
-                                <a href="#"><i class="fab fa-youtube"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-2 col-lg-3 col-md-4 col-sm-5">
-                 <div class="single-footer-caption mb-50">
-                     <div class="footer-tittle">
-                         <h4>Book Category</h4>
-                         <ul>  
-                             <li><a href="#">History</a></li>
-                             <li><a href="#">Horror - Thriller</a></li>
-                             <li><a href="#">Love Stories</a></li>
-                             <li><a href="#">Science Fiction</a></li>
-                             <li><a href="#">Business</a></li>
-                         </ul>
-                     </div>
-                 </div>
-             </div>
-             <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
-                 <div class="single-footer-caption mb-50">
-                     <div class="footer-tittle">
-                         <h4>&nbsp;</h4>
-                         <ul>
-                            <li><a href="#">Biography</a></li>
-                            <li><a href="#">Astrology</a></li>
-                            <li><a href="#">Digital Marketing</a></li>
-                            <li><a href="#">Software Development</a></li>
-                            <li><a href="#">Ecommerce</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6">
-             <div class="single-footer-caption mb-50">
-                 <div class="footer-tittle">
-                     <h4>Site Map</h4>
-                     <ul class="mb-20">
-                         <li><a href="#">Home</a></li>
-                         <li><a href="#">About Us</a></li>
-                         <li><a href="#">FAQs</a></li>
-                         <li><a href="#">Blog</a></li>
-                         <li><a href="#">Contact</a></li>
-                     </ul>
-                 </div>
-             </div>
-         </div>
-     </div>
- </div> 
-</div>
-<!-- footer-bottom area -->
-<div class="footer-bottom-area">
- <div class="container">
-     <div class="footer-border">
-         <div class="row d-flex align-items-center">
-             <div class="col-xl-12 ">
-                 <div class="footer-copy-right text-center">
-                     Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" style="color: black"target="_blank" rel="nofollow noopener">Colorlib</a>
-
-                 </div>
-             </div>
-         </div>
-     </div>
- </div>
-</div>
-</div>
-</footer>
+<?php
+    include("footer.php")
+?>
 <!-- Scroll Up -->
 <div id="back-top" >
     <a title="Go to Top" href="#"> <i class="fas fa-level-up-alt"></i></a>
 </div>
 
 <!-- JS here -->
+<script>
+
+
+</script>
 <!-- Jquery, Popper, Bootstrap -->
 <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
 <script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
