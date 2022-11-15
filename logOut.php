8b6d30b11d9a29cd5217a736924edb6ef610c814
Joe Ong <66934953+joeong01@users.noreply.github.com>
2022-11-15 10:42:03 +0800

    done

--- logOut.php
+++ logOut.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <?php
 session_start();
 session_unset();
@@ -9,16 +8,4 @@ echo "
 
     header('Location: http://localhost/Kid-s-bookstore/index.php');
 session_destroy();
-=======
-<?php
-session_start();
-session_unset();
-echo "
-    <script>
-        alert('Log Out and continued as guest');
-    </script>";
-
-    header('Location: http://localhost/Kid-s-bookstore/index.php');
-session_destroy();
->>>>>>> Stashed changes
 ?>
